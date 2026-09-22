3e56b41401f1e0820687a139f0cae7e5e702caa4
Flagship-One-Inc <weverson.barbieri@gmail.com>
2026-09-22 08:46:03 -0300

    fix lint issues on utils.py and main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ try:
 
     def create_df(worksheet):
         """Returns a pandas dataframe.
-         
+
         From the gspread.worksheet.Worksheet data type obtained with the function open_gsheet().
         Then, pandas DataFrame method converts gspread.worksheet.Worksheet to readable data.
 
@@ -18,12 +18,12 @@ try:
             worksheet (gspread.worksheet.Worksheet): When using the Google Cloud credentials to access a spresheet,
             the function open_gsheet() returns the first sheet as gspread.worksheet.Worksheet data type. 
 
-        Return: 
-            pandas.core.frame.DataFrame: Returns a dataframe with the format from pandas library from the gspread.worksheet.Worksheet data type
-            obtained with the function open_gsheet().
+        Return:
+            pandas.core.frame.DataFrame: Returns a dataframe with the format from pandas library from
+            the gspread.worksheet.Worksheet data type obtained with the function open_gsheet().
         """
         return pd.DataFrame(worksheet.get_all_records())
-    
+
     # Call the function to convert to dataframe
     df = create_df(worksheet)
 
@@ -37,29 +37,29 @@ try:
     part_number = part_number_input()
 
     def button_to_input_part_number(part_number):
-        """Returns a button below the text area. After pressing the button the part number is assigned 
+        """Returns a button below the text area. After pressing the button the part number is assigned
         to the variable part_number and then applies the regex pattern.
 
         As soon as the button is pressed, the part number inputted on the text area
         is assigned to the variable part_number. The part_number inputted becomes a regex pattern,
-        so that the part number inputted along with its variations can be extracted from the column 'OLD PART NUMBER(S)'.
+        so that the part number inputted along with its variations can be extracted from
+        column 'OLD PART NUMBER(S)'.
 
-        Arguments: 
+        Arguments:
             part_number (str): The string inputted on the text area on the browser.
 
-        Return: 
-            regex: Pattern after adding the part number inputted + regex. The regex are used to extract the part inputted
-            along with its variations. For example:
-
+        Return:
+            regex: Pattern after adding the part number inputted + regex. 
+            The regex are used to extract the part inputted along with its variations. 
+            For example:
             inputted part number: 1234
-
             extracted part numbers: 1234AA, 1234AB, 1234AC    
         """
         # Initialize pattern as None
         pattern = None
-        
+
         if st.button("Part Number Input"):
-            if len(part_number) < 8 :
+            if len(part_number) < 8:
                 st.write("Please, enter the correct part number.")
             else:
                 pattern = fr'((?:\n)?{re.escape(part_number)}[^\s]*)'
@@ -76,7 +76,7 @@ try:
 
     # Call the function to get a worksheet after updating hdw column
     worksheet_updated = open_gsheet()
-    
+
     # Call the function to convert the worksheet to dataframe after updating hdw column 
     df_updated = create_df(worksheet_updated)
 
--- utils.py
+++ utils.py
@@ -7,25 +7,25 @@ def open_gsheet():
 
     """Returns a gspread.worksheet.Worksheet data type.
 
-    It can be used to convert from this data type to a dataframe. 
-       
+    It can be used to convert from this data type to a dataframe.
+
     Arguments:
         json_path: The dir path where .json file with the Google Cloud credentials are stored. 
 
         gsheet_name: A string with the name of the gsheet.
 
-    Return: 
+    Return:
         Returns a gspread.worksheet.Worksheet data type where.
         It can be used to convert from this data type to a dataframe.
 
     """
 
     gsheet_name = "df_year_bc_es_extracted"
-    
+
     gc = gspread.service_account()
 
     sh = gc.open(gsheet_name)
-    
+
     worksheet = sh.sheet1
 
     return worksheet
@@ -34,18 +34,20 @@ def open_gsheet():
 def extract_update_cell(df, pattern, worksheet):
     """Returns the cell updated on the gsheet with the part numbers extracted from another cell.
     On the browser shows the cell where the part number were extracted along with all matches.
-    
+
     After remove the part number extracted from the cell where the part number was extracted.
 
     Argument: 
         df (pandas.core.frame.DataFrame): After opening the gsheet applying the function 'open_gsheet()',
-        followed by the conversion of the gspread.worksheet.Worksheet data type to DataFrame using the method .get_all_records().
-        pattern (regex): Regex converted from a single string to a pattern, so that the part number along with its variations
-        will be extracted.
-        worksheet (gspread.worksheet.Worksheet): The google sheet data type obtained when applying the function open_gsheet().
+        followed by the conversion of the gspread.worksheet.Worksheet data type to DataFrame using 
+        the method .get_all_records().
+        pattern (regex): Regex converted from a single string to a pattern, so that the 
+        part number along with its variations will be extracted.
+        worksheet (gspread.worksheet.Worksheet): The google sheet data type obtained when 
+        applying the function open_gsheet().
 
-    Return: 
-        Returns the cell updated on the gsheet with the part numbers extracted from the column OLD PART NUMBER(S), 
+    Return:
+        Returns the cell updated on the gsheet with the part numbers extracted from the column OLD PART NUMBER(S),
         remove the part number extracted from the cell where the part number was extracted.
     """
 
@@ -68,7 +70,7 @@ def extract_update_cell(df, pattern, worksheet):
                 df['OLD PART NUMBER(S)'][idx] = re.sub(pattern, '', cell_value_to_str)
                 cell_removed_pn = df['OLD PART NUMBER(S)'][idx]
                 worksheet.update(old_pn_cell, values=cell_removed_pn)
-                st.write(f'{old_pn_cell}: {', '.join(matches)}')      
+                st.write(f'{old_pn_cell}: {', '.join(matches)}')   
 
             # Condition when the cell on hdw column is no longer empty.
             elif matches and hdw_cell_value != "None":
